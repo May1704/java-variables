c4a61f3dcc68f12b41c9f9c2151bd9fb1938641b
May1704 <vergara.mayleris@gmail.com>
2025-05-07 05:53:51 +0200

    exercise Cakes done

--- Cakes.java
+++ Cakes.java
@@ -5,7 +5,8 @@ public class Cakes {
         int cake = 1;
         int people = 5;
 
-        //Cuando dividimos dos enteros cabe la posibilidad de que el resultado sea una fracción, pero no está bien cambiar a double algo que conceptualmente es un entero (como el pastel o las personas)
+        //Cuando dividimos dos enteros cabe la posibilidad de que el resultado sea una fracción, pero no está bien cambiar a
+        // double algo que conceptualmente es un entero (como el pastel o las personas)
         //Para que el resultado cambie de tipo usamos "type casting" y se hace agregando entre paréntesis lo que queremos convertir
         
         //Corre el siguiente código
@@ -13,23 +14,23 @@ public class Cakes {
         //El resultado obtenido es de tipo double.
         
 
-        //Ahora vamos a escribir un programa que convierta un valor de tipo double a int y de int a double. 
+        //Ahora vamos a escribir un programa que convierta un valor de tipo double a int y de int a double.
 
         //Declara y asigna un valor a una variable double
-        //Escribe aquí tu código
-        System.out.println("Valor original de double: <doubleValue>");
+        double doubleValue = 10.3;
+        System.out.println("Valor original de double:" + " " + doubleValue);
 
         //Convierte double a int
-        //Escribe aquí tu código
-        System.out.println("Valor convertido de double a int: <intValue>");
+        int intValue = (int) doubleValue;
+        System.out.println("Valor convertido de double a int:" + " " + intValue);
 
         //Declara y asigna un valor a una variable int
-        //Escribe aquí tu código
-        System.out.println("Valor original de int: <anotherIntValue>");
+        int anotherIntValue = 2024;
+        System.out.println("Valor original de int:" + " " + anotherIntValue);
 
         //Convierte int a double
-        //Escribe aquí tu código
-        System.out.println("Valor convertido de int a double: <anotherDoubleValue>");
+        double anotherDoubleValue = anotherIntValue;
+        System.out.println("Valor convertido de int a double:" + " " + anotherDoubleValue);
                
     }
 }
